83faf66d401cf59a2555d71f1d4326a05250180c
zala <andrewhisshion@gmail.com>
2022-03-18 11:20:12 +0800

    Edit #3

--- src/components/items.js
+++ src/components/items.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import spaceimg from "./space.jpg";
 
 function Items() {
     return (
         <>
             <li className="content_item">
+                    <figure className="content_item_pic-wrap">
+                        <img src={spaceimg} alt="cont" className="content_item_img"/>
+                    </figure>  
                     <div className="content_item_info">
-                        <h5 className="content_item_text">TEST TEST</h5>
+                        <h5 className="content_item_text"> This image is a development test img as is this whole site </h5>
                     </div>          
             </li>
         </>
