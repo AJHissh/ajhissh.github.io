2a4f99b61f0c97ee67698ee23212cc7c01aff13a
zala <andrewhisshion@gmail.com>
2022-03-24 15:34:14 +0800

    edit #16

--- src/components/pages/register.jsx
+++ src/components/pages/register.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import {useState, useEffect} from 'react';
+import {FcManager} from 'react-icons/fc';
+import "../register.css"
 
-function register() {
-  return (
-    <div>register</div>
-  )
+function Register() {
+  const [formData, SetFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    passwordconf: ''
+  })
+
+  const {name, email, password, passwordconf} = formData
+
+  const onChange = () => {}
+
+  return <>
+    <section className='heading'>
+      <h1>
+        <FcManager /> Register
+      </h1>
+    </section>
+    <section className='reg-form'>
+    <p> Fill in information to create account</p>
+      <form>
+        <div className='form-group'>
+        <input type="text" className="form-control" id="name" name="name" value={name} placeholder="Enter name" onChange={onChange} />
+        </div>
+        <div className='form-group'>
+        <input type="text" className="form-control" id="email" name="email" value={email} placeholder="Enter email" onChange={onChange} />
+        </div>
+        <div className='form-group'>
+        <input type="text" className="form-control" id="password" name="password" value={password} placeholder="Enter password" onChange={onChange} />
+        </div>
+        <div className='form-group'>
+        <input type="text" className="form-control" id="passwordconf" name="passwordconf" value={passwordconf} placeholder="Re-enter password" onChange={onChange} />
+        </div>
+      </form>
+    </section>
+  </>
 }
 
-export default register
+export default Register
