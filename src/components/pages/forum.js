87b3f7fd507d010b8b1020d55a75b3df63b4ee50
zala <andrewhisshion@gmail.com>
2022-04-01 22:58:20 +0800

    forum + others

--- src/components/pages/forum.js
+++ src/components/pages/forum.js
@@ -4,15 +4,42 @@ import '../forum.css';
 
 const Forum = () => {
     return (
-        <div className="forum-main">
-            <h1> </h1>
-            <div className="forum-text">
-                <h3>Forum coming soon!</h3>
-                <li>Sign up and Log in</li>
-                <li>Post messages in the forum</li>
+    <div className="form-cont">
+    <section className='heading-forum'>
+      <h1>
+         Forum
+      </h1>
+    </section>
+    <header>
+        <div class="forum-nav">
+            <nav class="navigation hide" id="navigation">
+                <span class="close-icon" id="close-icon" onclick="showIconBar()"><i class="fa fa-close"></i></span>
+                <ul class="nav-list">
+                    <li class="navfor-item"><a href="forums.html">Forums</a></li>
+                    <li class="navfor-item"><a href="posts.html">Posts</a></li>
+                    <li class="navfor-item"><a href="detail.html">Detail</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="search-box">
+            <div>
+                <select name="" id="">
+                    <option value="Everything">Everything</option>
+                    <option value="Titles">Titles</option>
+                    <option value="Descriptions">Descriptions</option>
+                </select>
+                <input type="text" name="q" placeholder="search ..."/>
+                <button><i class="fa fa-search"></i></button>
             </div>
         </div>
+    </header>
+
+    <footer className="foot-forum">
+        <span>&copy;  xdasdasd </span>
+    </footer>
+    </div>
     )
 }
+
 
 export default Forum;
