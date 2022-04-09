bda454b1d1b771f37ac3f177128c83e89fb4dbf8
zala <andrewhisshion@gmail.com>
2022-04-10 00:05:59 +0800

    gh pages test

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/AJHissh' element={<Home />} />
           <Route path='/About' element={<About />} />
           <Route path='/Forum' element={<Forum />} />
           <Route path='/login' element={<Login />} />
