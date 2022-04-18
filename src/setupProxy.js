3ef71b7b4d91e7cb7f652df3ad7b260177119ef2
zala <andrewhisshion@gmail.com>
2022-04-19 07:38:45 +0800

    small changes

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: 'https://ajh-website.herokuapp.com/',
       changeOrigin: true,
     })
   );
