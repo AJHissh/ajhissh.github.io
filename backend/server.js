40cad4009402cccfe085aceebdb424ac1ddd2dbd
zala <andrewhisshion@gmail.com>
2022-04-19 10:27:25 +0800

    test8

--- backend/server.js
+++ backend/server.js
@@ -18,10 +18,10 @@ app.use('/api/user-posts', require('./routes/user-posts'))
 app.use('/api/users', require('./routes/userRoutes'))
 
 if (process.env.NODE_ENV == "production") {
-    app.use(express.static(path.join(__dirname, "build")));
+    app.use(express.static("/app/build"));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.join('app/build', "index.html"));
+        res.sendFile("/app/build/index.html");
     })
 }
 
