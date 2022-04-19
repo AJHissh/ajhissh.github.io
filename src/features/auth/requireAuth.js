996b12e25c2df0529d649e71a2474b4999d6c2df
zala <andrewhisshion@gmail.com>
2022-04-19 12:54:20 +0800

    working edits #7

--- src/features/auth/requireAuth.js
+++ src/features/auth/requireAuth.js
@@ -2,10 +2,10 @@ import { useSelector } from "react-redux";
 import { Navigate, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import React from "react";
-import {useLocalState} from "../util/useLocalStorage";
+
 
 function RequireAuth({children}) {
-    const [jwt, setJwt] = useLocalState("", "jwt");
+    const [jwt, setJwt] = require('jsonwebtoken');
   return jwt ? children : <Navigate to="/login" />
   };
 
