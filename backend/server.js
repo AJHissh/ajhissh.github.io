0c907e9051207d48d9a98a822b8fd87dd8aa2e93
AHISSH <74668676+AJHissh@users.noreply.github.com>
2023-02-03 21:23:34 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const path = require('path')
 
-const port = process.env.PORT || 10000
+const port = process.env.PORT || 5000
 
 connectDB()
 
